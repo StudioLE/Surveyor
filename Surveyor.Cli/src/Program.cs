0ec0e9572b1746b4e95679ce357d1579d2d41897
Laurence Elsdon <git@elsdon.me>
2024-04-01 10:43:14 +0100

    feat(Surveyor.Cli): Revise CLI to evaluate command from arguments

--- Surveyor.Cli/src/Program.cs
+++ Surveyor.Cli/src/Program.cs
@@ -4,22 +4,55 @@ using Microsoft.Extensions.Options;
 using Surveyor.Hosting;
 using Surveyor.Versioning;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((_, services) => services
-        .AddSurveyorServices()
-        .AddSurveyorOptions())
-    .Build();
+namespace Surveyor.Cli;
 
-VersioningActivity activity = host.Services.GetRequiredService<VersioningActivity>();
-IOptions<VersioningActivityOptions> options = host.Services.GetRequiredService<IOptions<VersioningActivityOptions>>();
-SemanticVersion? versionQuery = await activity.Execute(options.Value);
-if (versionQuery is SemanticVersion version)
+public class Program
 {
-    Console.WriteLine(version.ToString());
-    Environment.Exit(0);
-}
-else
-{
-    Console.Error.WriteLine("Failed to determine the latest version of the package.");
-    Environment.Exit(1);
+
+    public static async Task Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((_, services) => services
+                .AddSurveyorServices()
+                .AddSurveyorOptions())
+            .Build();
+        string[] commands = args
+            .TakeWhile(x => !x.StartsWith('-'))
+            .ToArray();
+        if(commands.Length == 0)
+        {
+            Console.Error.WriteLine("No command specified.");
+            Environment.Exit(1);
+        }
+        Task task = commands.First() switch
+        {
+            "version" => ExecuteVersioning(host.Services),
+            _ => InvalidCommand()
+        };
+        await task;
+    }
+
+    private static async Task ExecuteVersioning(IServiceProvider services)
+    {
+        VersioningActivity activity = services.GetRequiredService<VersioningActivity>();
+        IOptions<VersioningActivityOptions> options = services.GetRequiredService<IOptions<VersioningActivityOptions>>();
+        SemanticVersion? versionQuery = await activity.Execute(options.Value);
+        if (versionQuery is SemanticVersion version)
+        {
+            Console.WriteLine(version.ToString());
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.Error.WriteLine("Failed to determine the latest version of the package.");
+            Environment.Exit(1);
+        }
+    }
+
+    private static Task InvalidCommand()
+    {
+        Console.Error.WriteLine("Invalid command.");
+        Environment.Exit(1);
+        return Task.CompletedTask;
+    }
 }
